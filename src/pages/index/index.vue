007bdfccffc36652fef5023f1ef6aa31b1c23412
HuangGreatd <13393711969@163.com>
2024-12-06 21:54:37 +0800

    前端按钮获取用户信息登录

---
+++ src/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<button class="button" @click="onWxLogin">微信授权登录</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+	const onWxLogin = () => {
+	  uni.login({
+	    provider: 'weixin',
+	    success: async (res) => {
+	      const data = await uni.request({
+	        method: "POST",
+	        url: "http://localhost:8080/api/user/auth/wechat",
+	        data: {
+	          code: res.code
+	        }
+	      })
+	      console.log(data);
+	
+	    },
+	  })
+	}
+	
+</script>
+
+
+
+<style scoped>
+	.button {
+		color: white;
+		padding: 6rpx;
+		background-color: #ED4556;
+		border-radius: none;
+	}
+</style>
